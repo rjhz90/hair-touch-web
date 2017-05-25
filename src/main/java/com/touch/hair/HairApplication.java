ac73fc51b6fce174fb8b02dd2be049ce66a74376
joohyeon-ryu <joohyeon-ryu@linecorp.com>
2017-05-25 15:25:14 +0900

    Add spring boot basic format

---
+++ src/main/java/com/touch/hair/HairApplication.java
@@ -0,0 +1,12 @@
+package com.touch.hair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairApplication.class, args);
+	}
+}
